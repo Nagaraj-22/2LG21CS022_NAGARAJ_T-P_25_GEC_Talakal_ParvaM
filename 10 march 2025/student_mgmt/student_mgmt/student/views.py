94a34499e6795d1b8724201dc75b3be231cc9377
Nagaraj <nagarajmaisalgi97@gmail.com>
2025-03-12 16:19:43 +0530

    new changes

---
+++ 10 march 2025/student_mgmt/student_mgmt/student/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .models import Student
+
+# Create your views here.
+def student_form(request):
+    if request.method =='POST':
+        sname = request.POST['sname']
+        sclass = request.POST['sclass']
+        ssection = request.POST['ssection']
+        sgender = request.POST['sgender']
+        sdob = request.POST['sdob']
+
+        Student.objects.create(sname=sname,
+        sclass=sclass, ssection=ssection,
+        sgender=sgender, sdob=sdob)
+    return render(request, 'students/student_form.html')
